93c50b761165210cd32363673acc244f5c8c9b21
MAMATA SHEE <67371648+geekytaurus115@users.noreply.github.com>
2021-09-13 20:12:31 +0530

    Create code.java

---
+++ Topic Wise/Arrays/Medium/189. Rotate Array/code.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        
+        // if k > length of array for that case
+        k = k % nums.length;
+        
+        //reverse 1st part
+        reverse(nums, 0, nums.length-k-1);
+        
+        //reverse 2nd part
+        reverse(nums, nums.length-k, nums.length-1);
+        
+        //now reverse the full arrary
+        reverse(nums, 0, nums.length-1);
+        
+    }
+    
+    
+     public static void reverse(int[] nums, int s, int e){
+	    
+	    int i = s;
+	    int j = e;
+	    
+	    while(i < j){
+	        int t = nums[i];
+	        nums[i] = nums[j];
+	        nums[j] = t;
+	        
+	        i++;
+	        j--;
+	    } 
+	}
+}
