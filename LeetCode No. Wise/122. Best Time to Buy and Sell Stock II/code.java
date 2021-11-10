e55d48522fc0e4987cbe40bf91964a575c7d6f07
MAMATA SHEE <67371648+geekytaurus115@users.noreply.github.com>
2021-11-10 12:25:42 +0530

    Create code.java

---
+++ LeetCode No. Wise/122. Best Time to Buy and Sell Stock II/code.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        if(prices == null || prices.length <= 1){
+            return 0;
+        }
+        
+        int sum = 0;
+        for(int i = 1; i < prices.length; i++){
+            
+            if(prices[i-1] < prices[i]){
+                sum += prices[i] - prices[i-1];
+            }
+        }
+        
+        return sum;
+        
+    }
+}
